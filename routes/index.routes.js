45438913113ab1d4d90352206171bf3ccd23bebe
binghatch <aron.seiffert@gmail.com>
2023-03-02 09:17:41 +0100

    feat: add /materials and /materials/:id routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,14 +4,40 @@ const router = express.Router();
 const RawMaterial = require('../models/RawMaterial.model');
 
 /* Post home page. */
-router.post('/', function(req, res, next) {
-  RawMaterial.create(req.body)
-    .then(rawMaterial => {
-      res.status(201).json(rawMaterial);
+// router.post('/', function(req, res, next) {
+//   RawMaterial.create(req.body)
+//     .then(rawMaterial => {
+//       res.status(201).json(rawMaterial);
+//     })
+//   .catch(error => {
+//     res.status(500).json(error);
+//   });
+// });
+
+
+router.get('/materials', (req, res, next) => {
+  RawMaterial.find()
+    .populate("primaryConstituents.byAmount")
+    .populate("primaryConstituents.byOlfactiveImpact")
+    .then(rawMaterials => {
+      res.status(200).json(rawMaterials);
     })
-  .catch(error => {
-    res.status(500).json(error);
-  });
+    .catch(error => {
+      res.status(500).json(error);
+    })
+});
+
+/* Get material by ID */
+router.get('/materials/:id', (req, res, next) => {
+  RawMaterial.findById(req.params.id)
+    .populate("primaryConstituents.byAmount")
+    .populate("primaryConstituents.byOlfactiveImpact")
+    .then(rawMaterial => {
+      res.status(200).json(rawMaterial);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 module.exports = router;
